637815afcabb619c93bf15f66c9ecbf5df012371
Basim Ghouri <97091625+ghourigeeks@users.noreply.github.com>
2024-05-07 21:10:18 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavBar from "./Components/NavBar";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Post from "./Components/Post";
+import ProtectedRoute from "./Components/ProtectedRoute";
+import { AuthProvider } from "./Components/AuthContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<><Header title="Basim Ghouri" img="home-bg.jpg" /><Home /></>} />
+          <Route path="/about" element={<><Header title="About Us" img="about-bg.jpg" /><About /></>} />
+          <Route path="/contact" element={<><Header title="Contact Us" img="contact-bg.jpg" /><Contact /></>} />
+          <Route
+            path="/post/*"
+            element={
+              <ProtectedRoute>
+                <>
+                  <Header title="Blog Post" img="post-bg.jpg" />
+                  <Post />
+                </>
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="*"
+            element={<><Header title="404 Not Found" img="contact-bg.jpg" /></>}
+          />
+        </Routes>
+        <Footer />
+      </Router>
+    </AuthProvider>
   );
 }
 
