a349301a0a5583be4a5b10a962f7c7fbd01eae41
Basim Ghouri <97091625+ghourigeeks@users.noreply.github.com>
2024-05-08 16:56:23 -0700

    Add Validation in Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,67 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export default function Contact () {
-    return (
-        <main className="mb-4">
-            <div className="container px-4 px-lg-5">
-                <div className="row gx-4 gx-lg-5 justify-content-center">
-                    <div className="col-md-10 col-lg-8 col-xl-7">
-                        <p>Want to get in touch? Fill out the form below to send me a message and I will get back to you as soon as possible!</p>
-                        <div className="my-5">
-                            {/* <!-- * * * * * * * * * * * * * * *-->
-                            <!-- * * SB Forms Contact Form * *-->
-                            <!-- * * * * * * * * * * * * * * *-->
-                            <!-- This form is pre-integrated with SB Forms.-->
-                            <!-- To make this form functional, sign up at-->
-                            <!-- https://startbootstrap.com/solution/contact-forms-->
-                            <!-- to get an API token!--> */}
-                            <form id="contactForm" data-sb-form-api-token="API_TOKEN">
-                                <div className="form-floating">
-                                    <input className="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
-                                    <label htmlFor="name">Name</label>
-                                    <div className="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
-                                </div>
-                                <div className="form-floating">
-                                    <input className="form-control" id="email" type="email" placeholder="Enter your email..." data-sb-validations="required,email" />
-                                    <label htmlFor="email">Email address</label>
-                                    <div className="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
-                                    <div className="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
-                                </div>
-                                <div className="form-floating">
-                                    <input className="form-control" id="phone" type="tel" placeholder="Enter your phone number..." data-sb-validations="required" />
-                                    <label htmlFor="phone">Phone Number</label>
-                                    <div className="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
-                                </div>
-                                <div className="form-floating">
-                                    <textarea className="form-control" id="message" placeholder="Enter your message here..." style={{height: "12rem"}} data-sb-validations="required"></textarea>
-                                    <label htmlFor="message">Message</label>
-                                    <div className="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
-                                </div>
-                                <br />
-                                {/* <!-- Submit success message-->
-                                <!---->
-                                <!-- This is what your users will see when the form-->
-                                <!-- has successfully submitted--> */}
-                                <div className="d-none" id="submitSuccessMessage">
-                                    <div className="text-center mb-3">
-                                        <div className="fw-bolder">Form submission successful!</div>
-                                        To activate this form, sign up at
-                                        <br />
-                                        <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
-                                    </div>
-                                </div>
-                                {/* <!-- Submit error message-->
-                                <!---->
-                                <!-- This is what your users will see when there is-->
-                                <!-- an error submitting the form--> */}
-                                <div className="d-none" id="submitErrorMessage"><div className="text-center text-danger mb-3">Error sending message!</div></div>
-                                {/* <!-- Submit Button--> */}
-                                <button className="btn btn-primary text-uppercase disabled" id="submitButton" type="submit">Send</button>
-                            </form>
-                        </div>
-                    </div>
+export default function Contact() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, phone, message } = formData;
+    if (!name || !email || !phone || !message) {
+      if (!name) {
+        toast.error('Name is required');
+      }
+      if (!email) {
+        toast.error('Email is required');
+      }
+      if (!phone) {
+        toast.error('Phone number is required');
+      }
+      if (!message) {
+        toast.error('Message is required');
+      }
+      return;
+    }
+    // Reset form fields after successful submission
+    setFormData({
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    });
+    // Simulate form submission or send data to the server
+    // You can add your submission logic here
+    toast.success("Form submitted successfully!");
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <main className="mb-4">
+      <div className="container px-4 px-lg-5">
+        <div className="row gx-4 gx-lg-5 justify-content-center">
+          <div className="col-md-10 col-lg-8 col-xl-7">
+            <p>
+              Want to get in touch? Fill out the form below to send me a message
+              and I will get back to you as soon as possible!
+            </p>
+            <div className="my-5">
+              <form onSubmit={handleSubmit}>
+                <div className="form-floating mb-3">
+                  <input
+                    className="form-control"
+                    id="name"
+                    type="text"
+                    placeholder="Enter your name..."
+                    name="name"
+                    value={formData.name}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="name">Name</label>
                 </div>
+                <div className="form-floating mb-3">
+                  <input
+                    className="form-control"
+                    id="email"
+                    type="email"
+                    placeholder="Enter your email..."
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="email">Email address</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <input
+                    className="form-control"
+                    id="phone"
+                    type="tel"
+                    placeholder="Enter your phone number..."
+                    name="phone"
+                    value={formData.phone}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor="phone">Phone Number</label>
+                </div>
+                <div className="form-floating mb-3">
+                  <textarea
+                    className="form-control"
+                    id="message"
+                    placeholder="Enter your message here..."
+                    style={{ height: "12rem" }}
+                    name="message"
+                    value={formData.message}
+                    onChange={handleChange}
+                  ></textarea>
+                  <label htmlFor="message">Message</label>
+                </div>
+                <button className="btn btn-primary text-uppercase" type="submit">
+                  Send
+                </button>
+              </form>
+              <ToastContainer />
             </div>
-        </main>
-    )
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
