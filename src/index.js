59f8d09ad80456c6f0f71abe1c7379e134d99d8c
Basim Ghouri <97091625+ghourigeeks@users.noreply.github.com>
2024-05-11 22:41:05 -0700

    Global Configuration for AXIOS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com/"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
