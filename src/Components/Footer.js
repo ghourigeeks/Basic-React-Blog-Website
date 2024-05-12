b9d367e8e1d04f8e44fac2e614db17dfb0da2def
Basim Ghouri <97091625+ghourigeeks@users.noreply.github.com>
2024-05-11 22:43:32 -0700

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
     return (
@@ -8,28 +9,28 @@ export default function Footer() {
                     <div className="col-md-10 col-lg-8 col-xl-7">
                         <ul className="list-inline text-center">
                             <li className="list-inline-item">
-                                <a href="#!">
+                                <Link href="#!">
                                     <span className="fa-stack fa-lg">
                                         <i className="fas fa-circle fa-stack-2x"></i>
                                         <i className="fab fa-twitter fa-stack-1x fa-inverse"></i>
                                     </span>
-                                </a>
+                                </Link>
                             </li>
                             <li className="list-inline-item">
-                                <a href="#!">
+                                <Link href="#!">
                                     <span className="fa-stack fa-lg">
                                         <i className="fas fa-circle fa-stack-2x"></i>
                                         <i className="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
                                     </span>
-                                </a>
+                                </Link>
                             </li>
                             <li className="list-inline-item">
-                                <a href="#!">
+                                <Link href="#!">
                                     <span className="fa-stack fa-lg">
                                         <i className="fas fa-circle fa-stack-2x"></i>
                                         <i className="fab fa-github fa-stack-1x fa-inverse"></i>
                                     </span>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                         <div className="small text-center text-muted fst-italic">Copyright &copy; Your Website 2023</div>
